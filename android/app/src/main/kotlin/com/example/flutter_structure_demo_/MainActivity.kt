5a648c9fff11b1a6d94084331584adcf732b0f9c
codeislife07 <ravisondarva7284@gmail.com>
2024-08-13 21:41:20 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_structure_demo_/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_structure_demo_
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/screens/home/widgets/banner_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BannerWidgetForHomeScreen extends StatelessWidget {
+  const BannerWidgetForHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
